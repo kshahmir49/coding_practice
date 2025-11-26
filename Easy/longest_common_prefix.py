978b8629d23dbf410fd2e2f9dcf1c0d529293e07
kshahmir49 <kshahmir49@gmail.com>
2025-11-26 12:39:59 +0400

    longest common prefix

---
+++ Easy/longest_common_prefix.py
@@ -0,0 +1,8 @@
+def longestCommonPrefix(self, strs):
+        res = ""
+        for i in range(len(strs[0])):
+            for j in range(1,len(strs)):
+                if i==len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return res
+            res += strs[0][i]
+        return res
